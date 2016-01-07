24f3535590709c54ebecb40b71df660227ea6e06
codejlay <jlay@outlook.com>
2016-01-07 13:41:00 -0800

    Squashed a small IE browser bug, intro image had a horizontal scroll bar

--- wp-content/themes/paw_ux/js/active-nav.js
+++ wp-content/themes/paw_ux/js/active-nav.js
@@ -1,8 +1,21 @@
-// Active Nav
+// Active Nav  ---As per the designer's request, menu underline has been temporary disabled--- 
 
-jQuery(function($){
-	 $('.menu a').click(function () {
-	         $('.menu a').removeClass('active');
-	          $(this).addClass('active');
-	       });
-})
+// jQuery(function($){
+// 	 $('.menu a').click(function () {
+// 	         $('.menu a').removeClass('active');
+// 	          $(this).addClass('active');
+// 	       });
+// })
+
+// jQuery(function($) {
+// 	$(window).on('scroll', function() {
+// 	    $('section').each(function() {
+// 	        if($(window).scrollTop() >= $(this).position().top -550) {
+// 	            var id = $(this).attr('id');
+// 	            $('.menu a').removeClass('active');
+// 	            $('.menu a[href=#'+ id +']').addClass('active');
+// 	        }
+// 	    })
+// 	})
+// });
+
